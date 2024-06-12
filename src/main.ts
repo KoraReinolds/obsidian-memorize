2646b5280778e8f595a80d0deeb55b795f7dfa60
KoraReinolds <s.shurin94@gmail.com>
2024-06-12 23:15:08 +0300

    parse memo codeblock

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { Plugin } from 'obsidian'
+import { parse } from 'yaml'
 
 export default class MemoPlugin extends Plugin {
   async onload() {
     console.log('Hi from memo plugin!')
+
+    this.registerMarkdownCodeBlockProcessor('memo', (source, el, ctx) => {
+      const settings = parse(source)
+      console.log(settings)
+    })
   }
 
   onunload() {}
